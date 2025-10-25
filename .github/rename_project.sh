2bec8c224c1e87e8e20826b04bfac32f96225785
bolin-d-wang <bolin-d-wang@users.noreply.github.com>
2025-10-25 02:31:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="bolin-d-wang"
+original_name="ofs_python_template"
+original_urlname="ofs-python-template"
+original_description="Awesome ofs_python_template created by bolin-d-wang"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ofs_python_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ ofs_python_template/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for ofs_python_template."""
+
+from ofs_python_template.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
